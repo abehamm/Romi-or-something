4146c267071b11fa80ffacc6dae44f16633ae99f
abehamm <abebaltodanojr@gmail.com>
2024-08-20 17:33:52 -0700

    added romi project or added romi code

---
+++ Romi Robot/src/main/java/frc/robot/Drive.java
@@ -0,0 +1,73 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Drive {
+    
+  public final Spark left_motor = new Spark(0); // creates a Spark object for the left motor
+  public final Spark right_motor = new Spark(1); // creates a Spark object for the right motor
+  private final Encoder left_encoder = new Encoder(4,5); //creates an Encoder object for the left encoder;
+  private final Encoder right_encoder = new Encoder(6,7); // creates an Encoder object for the right encoder;
+
+  /***
+   * This method will reset the encoder at startup
+   * to assure that we have travled 0 distance.
+   * Also changes the units we measure distance from ticks to meters
+   */
+  public Drive(){
+
+    left_encoder.reset();
+    right_encoder.reset();
+    left_encoder.setDistancePerPulse((70*Math.PI) / 1000 / 1440);
+    right_encoder.setDistancePerPulse((70*Math.PI) / 1000 / 1440);
+  }
+
+  /***
+   * 
+   * @param speed 
+   *  speed that we set the motor to
+   * @param turn 
+   *  the displacement of the turn to either slow or speed up the wheel
+   */
+  public void driveWheels(double speed, double turn){
+    left_motor.set(speed + turn); // calculation which adds the speed and turn parameters to set the speed of the left wheel
+    right_motor.set(speed - turn); // calculatino which subtracts the turn from the speed to set the speed of the right wheel
+  }
+
+  // 1 revolution = 1440
+  // wheel diameter = 70 mm
+  /***
+   * this method grabs the distances from the encoders and puts them into the smartdashboard for the user to see the information
+   */
+  public void getDistances(){
+
+    SmartDashboard.putNumber("Left Distance (M): ", left_encoder.getDistance());
+    SmartDashboard.putNumber("Right Distance (M): ", right_encoder.getDistance());
+  }
+
+  /***
+   * this method resets the distances that were tracked on the encoder
+   */
+  public void resetDistances(){
+
+    left_encoder.reset();
+    right_encoder.reset();
+  }
+
+  /***
+   * 
+   * this method alters the speed and turning variables if we wanted to change the rate of each param
+   * 
+   * @param speed // speed we want the romi to drive at
+   * @param turn // offset that we want to add or subtract to one of the wheels to turn them
+   * @param speedMag // amplifies the rate of speed
+   * @param turnMag // amplifies the turning speed
+   */
+  public void driveWheels(double speed, double turn, double speedMag, double turnMag){
+    left_motor.set(speed*speedMag + turn*turnMag);
+    right_motor.set(speed*speedMag - turn*turnMag);
+  }
+}
---
+++ Romi Robot/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,125 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  /***
+   * joystick object that is used to control romi
+   */
+  private final Joystick joystick = new Joystick(0); // create a Joystick object to control romi
+  /***
+   * drive object to access the fields within it
+   */
+  private final Drive drive = new Drive();
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+
+    drive.right_motor.setInverted(true);
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+
+    drive.getDistances();
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+
+
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+
+    if (joystick.getRawButtonPressed(1)){ // checks to see if the A button has been pressed on the controller
+      drive.resetDistances(); // if it was pressed, reset the encoders travled distance and set them to 0;
+    }
+
+    drive.driveWheels(joystick.getRawAxis(1), joystick.getRawAxis(4), 0.25, 0.25); /*method we implemented that takes the value 
+    of the joysticks we are driving with, to move both wheels, and then multiply the speed of the wheels by 0.25 to make them slower. */
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+}
